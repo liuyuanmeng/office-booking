b85545bfab8a14ccf7e97f985150d1a41a424921
liuyuanmeng <100864042+liuyuanmeng@users.noreply.github.com>
2024-07-19 14:26:38 +0100

    Delete src/main directory

--- src/main/java/Daisy/officeapp/repository/SpaceRepository.java
+++
@@ -1,9 +0,0 @@
-package Daisy.officeapp.repository;
-
-import Daisy.officeapp.model.Space;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpaceRepository extends JpaRepository<Space, Long> {
-}
